e8770b29ff369cdc72bf9e70d221e420573b7a0e
ksk0823 <machine0823@naver.com>
2024-06-20 01:13:11 +0900

    차트 기능 초단위로 수정

--- app/src/main/java/com/example/concentree/screens/ChartScreen.kt
+++ app/src/main/java/com/example/concentree/screens/ChartScreen.kt
@@ -1,13 +1,9 @@
 package com.example.concentree.screens
 
 import android.util.Log
-import android.widget.CalendarView
-import android.widget.Space
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,18 +17,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
@@ -45,36 +35,29 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.concentree.R
 import com.example.concentree.roomDB.ForestTree
-import com.example.concentree.roomDB.Tree
 import com.example.concentree.viewmodel.AppViewModel
 import kotlinx.coroutines.launch
 import java.time.DayOfWeek
+import java.time.Duration
 import java.time.LocalDate
-import java.time.YearMonth
 import java.time.LocalDateTime
 import java.time.LocalTime
-import androidx.compose.runtime.livedata.*
-import androidx.compose.ui.platform.LocalContext
-import androidx.room.PrimaryKey
-import java.time.Duration
+import java.time.YearMonth
 
 
 @Composable
@@ -389,7 +372,7 @@ fun BarChart(entries: List<ForestTree>) {
     val maxValue = weekData.maxOfOrNull { dayList ->
         dayList.sumOf {
 //            Log.d("qwqw", Duration.between(it.startTime, it.endTime).toMinutes().toString())
-            Duration.between(it.startTime, it.endTime).toMinutes()
+            Duration.between(it.startTime, it.endTime).getSeconds()
         }
     } ?: 0
 
@@ -407,7 +390,7 @@ fun BarChart(entries: List<ForestTree>) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 val sum = entry.sumOf {
 //                    Log.d("qwqw", "as: ${Duration.between(it.startTime, it.endTime).toMinutes()}")
-                    Duration.between(it.startTime, it.endTime).toMinutes()
+                    Duration.between(it.startTime, it.endTime).getSeconds()
                 }
 
                 val heightFraction = if (maxValue.toInt() != 0) sum.toFloat() / maxValue else 0f
